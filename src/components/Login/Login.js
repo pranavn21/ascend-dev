e37576cc35b89d506152b880c7bf5dd5b34e8fea
Kousthub Ganugapati <kousthub.ganugapati.738@k12.friscoisd.org>
2024-11-16 18:42:39 -0600

    register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import './Login.css';
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -12,8 +12,26 @@ const Login = () => {
 
   return (
     <div>
-      <form action="/submit" method="POST">
+      <form action="/submit" method="POST" name="loginForm">
         <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+        <br /><br />
+
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+        <br /><br />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+        <br /><br />
+
+        <button type="submit">Submit</button>
+      </form>
+      <h2>
+        If you do not have an account register here
+      </h2>
+      <form>
+      <label htmlFor="name">Name:</label>
         <input type="text" id="name" name="name" required />
         <br /><br />
 
